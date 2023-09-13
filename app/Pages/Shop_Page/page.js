ac53228b9a0b14186c53575c9c0666dd130e09ee
Sudarshan_Bhalbar <sudarshan.bhalbar@gmail.com>
2023-09-13 19:13:34 +0530

    Shop Page improvement
    Some changes in card section

--- app/Pages/Shop_Page/page.js
+++ app/Pages/Shop_Page/page.js
@@ -1,20 +1,51 @@
 import Image from "next/image";
 import { FaHeart } from "react-icons/fa";
+import { CgShoppingCart } from "react-icons/cg";
 import { useState } from "react";
 import Images from "@/app/Components/Images";
+import Img1 from "@/app/images/model2.svg"
 
 const ShopPage = () => {
   const [likes, setLikes] = useState(Array(8).fill(false));
+  const [ItemImageSrc, setItemImageSrc] = useState(Images.map((e) => e.ItemImageSrc[0]));
+
+  const handleMouseOver = (index) => {
+    const newItemImageSrc = [...ItemImageSrc];
+    newItemImageSrc[index] = Images[index].ItemImageSrc[1];
+    setItemImageSrc(newItemImageSrc);
+  };
+
+  const handleMouseOut = (index) => {
+    const newItemImageSrc = [...ItemImageSrc];
+    newItemImageSrc[index] = Images[index].ItemImageSrc[0];
+    setItemImageSrc(newItemImageSrc);
+  };
 
   const handleClick = (index) => {
     const newLikes = [...likes];
     newLikes[index] = !newLikes[index];
     setLikes(newLikes);
   };
+  
+
   return (
     <>
       <div id="shop-page">
         <h1 className="no-select">Shop</h1>
+        <div id="shop-image-div">
+          <div id="Image-div">
+            <Image  src={Img1} objectPosition="center" objectFit="cover" />
+          </div>
+          <div id="shop-text" >
+            <p className="no-select">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate molestiae delectus iusto deleniti!</p>
+            <div id="shop-Btn" className="no-select">Shop Now</div>
+          </div>
+          <div id="shop-highlight-text" >
+            <p className="no-select">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, consequuntur iure. Quo, accusamus?</p>
+            
+          </div>
+        </div>
+        <h1 className="no-select">New Arrivals</h1>
         <div id="shop-page-elements">
           {Images.map((e, index) => {
             const {
@@ -23,34 +54,40 @@ const ShopPage = () => {
               Itemtype,
               ItemSize,
               ItemPrice,
-              ItemImageSrc,
             } = e;
-            console.log(ItemImageSrc);
-            return(
-              <div id="shop-elements" key={Id}>
-              <FaHeart
-                id="heart"
-                onClick={() => handleClick(Id)}
-                style={{ color: likes[Id] ? "#c01e1e" : "#FFFFF6", zIndex:99 }}
-              />
-              <div id="image-container">
-                <Image
-                  src={ItemImageSrc}
-                  alt=""
-                  layout="fill"
-                  objectFit="cover"
-                  objectPosition="center"
+            return (
+              <div id="shop-elements" key={index}>
+                <FaHeart
+                  id="heart"
+                  onClick={() => handleClick(index)}
+                  style={{
+                    color: likes[index] ? "#ff00008e" : "#a2a2d2",
+                    zIndex: 99,
+                  }}
                 />
+                  <div id="image-container"
+                  onMouseOver={() => handleMouseOver(index)}
+                  onMouseOut={() => handleMouseOut(index)}
+                  >
+                    <Image
+                      src={ItemImageSrc[index]}
+                      alt=""
+                      layout="fill"
+                      objectFit="cover"
+                      objectPosition="center"
+                    />
+                  </div>
+                  <div id="cart-btn">
+                    <CgShoppingCart id="cart-icon"/>
+                  </div>
               </div>
-              <h2>{ItemName}</h2>
-            </div>
-            )
-            
+            );
           })}
         </div>
       </div>
     </>
   );
 };
+
 
 export default ShopPage;
--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["unsplash.com", "images.unsplash.com"],
+        domains: ["unsplash.com", "images.unsplash.com","lp2.hm.com"],
       }
 }
 
