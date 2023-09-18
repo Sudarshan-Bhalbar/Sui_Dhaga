30e8877f1769123ff8349c4967c1f6577984ad81
Sudarshan_Bhalbar <sudarshan.bhalbar@gmail.com>
2023-09-18 19:06:48 +0530

    navbar modify
    Give a scrolling behaviour to navbar

--- app/Components/Navbar.js
+++ app/Components/Navbar.js
@@ -2,13 +2,36 @@
 import { motion } from "framer-motion";
 import { RiMenu3Line } from "react-icons/ri";
 import { AnimatePresence } from "framer-motion";
+import { useState, useEffect } from 'react';
 
 const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.pageYOffset;
+      if (scrollTop > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
 
   return (
     <>
       <AnimatePresence>
-        <div  id="Navbar" className=" fixed z-50">
+        <motion.div  id="Navbar" className={`navbar fixed z-50 ${isScrolled ? 'navbar--scrolled' : ''}`}
+      initial={{ backgroundColor: 'transparent' }}
+      animate={{ backgroundColor: isScrolled ? 'rgba(255, 255, 255, 0.5)' : 'transparent' }}
+      transition={{ duration: 0.3 }}>
           <div id="nav-l" >
             <motion.h3>
               SUI DHAGA
@@ -17,7 +40,7 @@ const Navbar = () => {
           <div id="nav-r">
             <RiMenu3Line id="nav-r-menu-bar" />
           </div>
-        </div>
+        </motion.div>
       </AnimatePresence>
     </>
   );
