04330b3bb8af700cc403c3dc96d51f73585cf9fe
Sudarshan_Bhalbar <sudarshan.bhalbar@gmail.com>
2023-11-04 00:02:16 +0530

    s

--- app/Components/Images.js
+++ app/Components/Images.js
@@ -1,11 +1,10 @@
 const Items = [
   {
-    
     Id: 1,
     ItemName: "Jersey trousers",
     ItemDesc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    Itemtype: "Upper",
-    ItemSize: ["SM","M","L","XL","XXL","XXXL"],
+    Itemtype: "Women",
+    ItemSize: ["SM", "M", "L", "XL", "XXL", "XXXL"],
     ItemPrice: "Rs. 1,499.00",
     ItemImageSrc: [
       "https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2Fe1%2F51%2Fe151f22058e91ac8f21d1f0236eb21cc9c431290.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]",
@@ -16,8 +15,8 @@ const Items = [
     Id: 2,
     ItemName: "V-neck blouse",
     ItemDesc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    Itemtype: "",
-    ItemSize: ["SM","M","L","XL","XXL","XXXL"],
+    Itemtype: "Women",
+    ItemSize: ["SM", "M", "L", "XL", "XXL", "XXXL"],
     ItemPrice: "Rs. 1,599.00",
     ItemImageSrc: [
       "https://lp2.hm.com/hmgoepprod?set=format%5Bwebp%5D%2Cquality%5B79%5D%2Csource%5B%2Fdd%2Fd6%2Fddd64901b905c93761d653bcd7ee224135c8f7be.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url%5Bfile%3A%2Fproduct%2Fmain%5D",
@@ -28,8 +27,8 @@ const Items = [
     Id: 3,
     ItemName: "Rib-knit jumper",
     ItemDesc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    Itemtype: "",
-    ItemSize: ["SM","M","L","XL","XXL","XXXL"],
+    Itemtype: "Women",
+    ItemSize: ["SM", "M", "L", "XL", "XXL", "XXXL"],
     ItemPrice: "Rs. 2,999.00",
     ItemImageSrc: [
       "https://lp2.hm.com/hmgoepprod?set=format%5Bwebp%5D%2Cquality%5B79%5D%2Csource%5B%2Ff6%2F85%2Ff68534609bb31f508d6c968ae3bd7ac2d03dee98.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url%5Bfile%3A%2Fproduct%2Fmain%5D",
@@ -40,8 +39,8 @@ const Items = [
     Id: 4,
     ItemName: "Slim Mom High Ankle Jeans",
     ItemDesc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    Itemtype: "",
-    ItemSize: ["SM","M","L","XL","XXL","XXXL"],
+    Itemtype: "Women",
+    ItemSize: ["SM", "M", "L", "XL", "XXL", "XXXL"],
     ItemPrice: "Rs. 3,499.00",
     ItemImageSrc: [
       "https://lp2.hm.com/hmgoepprod?set=format%5Bwebp%5D%2Cquality%5B79%5D%2Csource%5B%2Fac%2F33%2Fac33c033d44e6e37aabfa44cf7d06bfc5545b8ef.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url%5Bfile%3A%2Fproduct%2Fmain%5D",
@@ -52,8 +51,8 @@ const Items = [
     Id: 5,
     ItemName: "Pendant earrings",
     ItemDesc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    Itemtype: "",
-    ItemSize: ["SM","M","L","XL","XXL","XXXL"],
+    Itemtype: "Women",
+    ItemSize: ["SM", "M", "L", "XL", "XXL", "XXXL"],
     ItemPrice: "Rs. 5,499.00",
     ItemImageSrc: [
       "https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F25%2F72%2F2572da567009c0b550805cfb62aab91698c0a72c.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]",
@@ -64,19 +63,14 @@ const Items = [
     Id: 6,
     ItemName: "Oversized printed T-shirt",
     ItemDesc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    Itemtype: "",
-    ItemSize: ["SM","M","L","XL","XXL","XXXL"],
+    Itemtype: "Women",
+    ItemSize: ["SM", "M", "L", "XL", "XXL", "XXXL"],
     ItemPrice: "Rs. 1,499.00",
     ItemImageSrc: [
       "https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2Fc6%2Fab%2Fc6ab5b0f42b01b7d7470e1046e3c2163038b1a8b.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bladies_tops_printed_tshirts%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]",
       "https://lp2.hm.com/hmgoepprod?set=format%5Bwebp%5D%2Cquality%5B79%5D%2Csource%5B%2F49%2Fc9%2F49c9883c6a307e945c3adebad7d3708a784505d7.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bladies_tops_printed_tshirts%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url%5Bfile%3A%2Fproduct%2Fmain%5D",
     ],
   },
-  {
-    Image:"https://plus.unsplash.com/premium_photo-1661315496157-39c0eb6ba247?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGdyb3VwJTIwZGlzY3Vzc2lvbnxlbnwwfDB8MHx8fDA%3D"
-  },
 ];
-
-
 
 export default Items;
