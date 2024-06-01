f30398424d22059e8e5e7b493c9b46f7a0206450
Sudarshan_Bhalbar <sudarshan.bhalbar@gmail.com>
2024-06-01 15:36:33 +0530

    update

--- app/Components/Images.js
+++ app/Components/Images.js
@@ -7,7 +7,7 @@ const Items = [
     ItemSize: ["SM", "M", "L", "XL", "XXL", "XXXL"],
     ItemPrice: "Rs. 1,499.00",
     ItemImageSrc: [
-      "https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2Fe1%2F51%2Fe151f22058e91ac8f21d1f0236eb21cc9c431290.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]",
+      "https://lp2.hm.com/hmgoepprod?set=format%5Bwebp%5D%2Cquality%5B79%5D%2Csource%5B%2F46%2F3e%2F463ed34cd306eb5825208ad2b24ad4f6b5681c61.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url%5Bfile%3A%2Fproduct%2Fmain%5D",
       "https://lp2.hm.com/hmgoepprod?set=format%5Bwebp%5D%2Cquality%5B79%5D%2Csource%5B%2F46%2F3e%2F463ed34cd306eb5825208ad2b24ad4f6b5681c61.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url%5Bfile%3A%2Fproduct%2Fmain%5D",
     ],
   },
